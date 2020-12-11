ec213805f8ebe46da60ea4d16e6678f82b97df36
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-11 23:54:57 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,17 +12,6 @@ class Customer
   def self.all
     @@all
   end
- 
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self
-    end
-  end
- 
-  def waiters
-    meals.map do |meal|
-      meal.waiter
-    end
     
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip=0)
